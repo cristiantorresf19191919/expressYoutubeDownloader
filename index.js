529876b32392d108d5a7550d6463c44834e50a92
Cristian Torres <cristian.torres@techdata.com>
2021-12-13 23:54:13 -0500

    Add static files html properly

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
+var path = require('path')
+process.env.PWD = process.cwd();
 app.use(cors());
 app.listen(4000, () => {
     console.log('Server Works !!! At port 4000');
 });
-app.use(express.static('public'));
+app.use(express.static(path.join(process.env.PWD, 'public')));
 app.get('/download', (req,res) => {
 var URL = req.query.URL;
 res.header('Content-Disposition', 'attachment; filename="video.mp4"');
